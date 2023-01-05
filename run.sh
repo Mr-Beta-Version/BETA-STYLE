296ee55eb163204092207c42f158185699a64c61
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-01-05 22:30:49 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ echo "INSTALLING THEME AND COLOURS"
 mv * $HOME/.termux
 rm -rf $HOME/BETA-STYLE
 rm $HOME/.termux/run.sh $HOME/.termux/README.md
-read -p "Enter Your Name > " NAME
+echo "Enter Your Name > "
+read NAME
 xdg-open "https://patorjk.com/software/taag/#p=display&f=ANSI Shadow&t=$NAME"
 sleep 5
 nano /data/data/com.termux/files/usr/etc/motd
