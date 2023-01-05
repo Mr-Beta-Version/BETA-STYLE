37aba8c13644cad7ed1685570562fec70ba7bd59
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-01-05 22:27:27 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mv * $HOME/.termux
 rm -rf $HOME/BETA-STYLE
 rm $HOME/.termux/run.sh $HOME/.termux/README.md
 read -p "Enter Your Name > " NAME
-xdg-open https://patorjk.com/software/taag/#p=display&f=ANSI Shadow&t=$NAME
+xdg-open "https://patorjk.com/software/taag/#p=display&f=ANSI Shadow&t=$NAME"
 nano /data/data/com.termux/files/usr/etc/motd
 echo "INSTALL SUCCESSFUL"
 kill -KILL $PPID
